397e5c66573786fa2f859986a0694581a33b2738
Tanish Mishra <tanishmishra97@gmail.com>
2023-12-09 21:10:58 +0530

    fix(next-button): fixed next button forward issue

--- script.js
+++ script.js
@@ -202,50 +202,51 @@ song_card_play_btn.forEach((element, index, allSong) => {
     //   }
     //   //  when inside the previous song is clicked
     // });
-  
+
     play_btn.style.visibility = "visible";
-    
+
     let currentSong = tempSong;
     let currentSongIndex = null;
     nextSong.addEventListener("click", () => {
-     
       play_btn.style.visibility = "hidden";
       if (music.currentTime > 0 || music.paused || music.currentTime <= 0) {
-     
-      // console.log(currentSongIndex)
-      for(let i = 0; i<songs.length;i++) {
-        if(currentSong == songs[i].songPath) {
-         currentSongIndex = i;
+        // console.log(currentSongIndex)
+        for (let i = 0; i < songs.length; i++) {
+          if (currentSong == songs[i].songPath) {
+            currentSongIndex = i;
+            console.log(currentSongIndex);
+            currentSongIndex = currentSongIndex + 1;
+            console.log(currentSongIndex);
+            break;
+          }
         }
-      }
-      currentSongIndex = currentSongIndex + 1;
-      console.log(currentSongIndex)
-      if(currentSongIndex >= songs.length - 1) {
-        currentSongIndex = songs.length - 1;
-      }
-      music.src = songs[currentSongIndex].songPath;
-      music.play();
-      song_name.innerHTML = songs[currentSongIndex].songName;
+
+        console.log(currentSongIndex);
+        if (currentSongIndex >= songs.length - 1) {
+          currentSongIndex = songs.length - 1;
+        }
+        music.src = songs[currentSongIndex].songPath;
+        music.play();
+        song_name.innerHTML = songs[currentSongIndex].songName;
         artist_name.innerHTML = songs[currentSongIndex].artistName;
         song_card_play_btn.forEach((element, index) => {
           element.innerHTML = '<i class="ri-play-circle-line"></i>';
         });
-        allSong[currentSongIndex].innerHTML = '<i class="ri-pause-circle-fill"></i>';
+        allSong[currentSongIndex].innerHTML =
+          '<i class="ri-pause-circle-fill"></i>';
         tempSong = songs[currentSongIndex].songPath;
-        console.log(tempSong)
-    }
-     
+        currentSong = tempSong;
+      }
     });
-   
 
-
+  
   });
 });
 
 // To set the progress bar value 0 everytime when we play the song
-music.addEventListener('play',()=> {
+music.addEventListener("play", () => {
   progress_bar.value = 0;
-})
+});
 
 // To Update the Progress Bar current time is in percentage
 music.addEventListener("timeupdate", () => {
